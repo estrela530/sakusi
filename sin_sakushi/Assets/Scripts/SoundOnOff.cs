04894374e8a8c772e90c597eb43d8ac918312ee1
estrela530 <daisuke.0530.otk@gmail.com>
2020-04-08 03:53:28 +0900

    音完成

---
+++ sin_sakushi/Assets/Scripts/SoundOnOff.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundOnOff : MonoBehaviour
+{
+    [SerializeField]
+    public AudioClip audioClip;
+    [SerializeField]
+    public AudioSource audioSource;
+    [SerializeField]
+    public IgnitStatus ignitStatus;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = gameObject.GetComponent<AudioSource>();
+        audioSource.clip = audioClip;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ignitStatus = gameObject.GetComponent<IgnitStatus>();
+        if (ignitStatus.GetIgnit() == false)
+        {
+            audioSource.Play();
+        }
+    }
+}
